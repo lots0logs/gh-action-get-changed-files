27d7b755242708cb7d16f4d9f0ece810b1414e6f
Yevhen Fabizhevskyi <fabasoad@gmail.com>
2020-03-17 15:28:20 +0900

    Added 'renamed' status

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const FILES          = [];
 const FILES_MODIFIED = [];
 const FILES_ADDED    = [];
 const FILES_DELETED  = [];
+const FILES_RENAMED  = [];
 
 const gh   = new GitHub(core.getInput('token'));
 const args = { owner: owner.name, repo: repo.name };
@@ -28,6 +29,10 @@ function isModified(file) {
 	return 'modified' === file.status;
 }
 
+function isRenamed(file) {
+	return 'renamed' === file.status;
+}
+
 async function processCommit(commit) {
 	args.ref = commit.id;
 	result   = await gh.repos.getCommit(args);
@@ -38,10 +43,12 @@ async function processCommit(commit) {
 		files.forEach( file => {
 			isModified(file) && FILES.push(file.filename);
 			isAdded(file) && FILES.push(file.filename);
+			isRenamed(file) && FILES.push(file.filename);
 
 			isModified(file) && FILES_MODIFIED.push(file.filename);
 			isAdded(file) && FILES_ADDED.push(file.filename);
 			isDeleted(file) && FILES_DELETED.push(file.filename);
+			isRenamed(file) && FILES_RENAMED.push(file.filename);
 		});
 	}
 }
@@ -53,11 +60,13 @@ Promise.all(commits.map(processCommit)).then(() => {
 	process.stdout.write(`::set-output name=added::${JSON.stringify(FILES_ADDED, 4)}`);
 	process.stdout.write(`::set-output name=deleted::${JSON.stringify(FILES_DELETED, 4)}`);
 	process.stdout.write(`::set-output name=modified::${JSON.stringify(FILES_MODIFIED, 4)}`);
+	process.stdout.write(`::set-output name=renamed::${JSON.stringify(FILES_RENAMED, 4)}`);
 
 	fs.writeFileSync(`${process.env.HOME}/files.json`, JSON.stringify(FILES), 'utf-8');
 	fs.writeFileSync(`${process.env.HOME}/files_modified.json`, JSON.stringify(FILES_MODIFIED), 'utf-8');
 	fs.writeFileSync(`${process.env.HOME}/files_added.json`, JSON.stringify(FILES_ADDED), 'utf-8');
 	fs.writeFileSync(`${process.env.HOME}/files_deleted.json`, JSON.stringify(FILES_DELETED), 'utf-8');
+	fs.writeFileSync(`${process.env.HOME}/files_renamed.json`, JSON.stringify(FILES_RENAMED), 'utf-8');
 
 	process.exit(0);
 });
