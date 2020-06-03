06a976f18668d6d211951f393cc183cb448d3a40
Dustin Falgout <dustin@falgout.us>
2020-06-03 14:39:12 -0500

    Add proper support for pull_request event. Fixes #8

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ const fs                  = require('fs');
 const { context, GitHub } = require('@actions/github');
 const core                = require('@actions/core');
 
-const commits = context.payload.commits.filter(c => c.distinct);
 const repo    = context.payload.repository;
 const org     = repo.organization;
 const owner   = org || repo.owner;
 
-const FILES          = [];
-const FILES_MODIFIED = [];
-const FILES_ADDED    = [];
-const FILES_DELETED  = [];
-const FILES_RENAMED  = [];
+const FILES          = new Set();
+const FILES_MODIFIED = new Set();
+const FILES_ADDED    = new Set();
+const FILES_DELETED  = new Set();
+const FILES_RENAMED  = new Set();
 
 const gh   = new GitHub(core.getInput('token'));
 const args = { owner: owner.name, repo: repo.name };
@@ -33,6 +32,17 @@ function isRenamed(file) {
 	return 'renamed' === file.status;
 }
 
+async function getCommits() {
+	if ('push' === context.eventName) {
+		return context.payload.commits;
+
+	} else if ('pull_request' === context.eventName) {
+		const url = context.payload.pull_request.commits_url;
+
+		return gh.paginate(`GET ${url}`);
+	}
+}
+
 async function processCommit(commit) {
 	args.ref = commit.id;
 	result   = await gh.repos.getCommit(args);
@@ -41,32 +51,37 @@ async function processCommit(commit) {
 		const files = result.data.files;
 
 		files.forEach( file => {
-			isModified(file) && FILES.push(file.filename);
-			isAdded(file) && FILES.push(file.filename);
-			isRenamed(file) && FILES.push(file.filename);
+			isModified(file) && FILES.add(file.filename);
+			isAdded(file) && FILES.add(file.filename);
+			isRenamed(file) && FILES.add(file.filename);
 
-			isModified(file) && FILES_MODIFIED.push(file.filename);
-			isAdded(file) && FILES_ADDED.push(file.filename);
-			isDeleted(file) && FILES_DELETED.push(file.filename);
-			isRenamed(file) && FILES_RENAMED.push(file.filename);
+			isModified(file) && FILES_MODIFIED.add(file.filename);
+			isAdded(file) && FILES_ADDED.add(file.filename);
+			isDeleted(file) && FILES_DELETED.add(file.filename);
+			isRenamed(file) && FILES_RENAMED.add(file.filename);
 		});
 	}
 }
 
+getCommits().then(commits => {
+	commits = commits.filter(c => c.distinct);
 
-Promise.all(commits.map(processCommit)).then(() => {
-	process.stdout.write(`::debug::${JSON.stringify(FILES, 4)}`);
-	process.stdout.write(`::set-output name=all::${JSON.stringify(FILES, 4)}`);
-	process.stdout.write(`::set-output name=added::${JSON.stringify(FILES_ADDED, 4)}`);
-	process.stdout.write(`::set-output name=deleted::${JSON.stringify(FILES_DELETED, 4)}`);
-	process.stdout.write(`::set-output name=modified::${JSON.stringify(FILES_MODIFIED, 4)}`);
-	process.stdout.write(`::set-output name=renamed::${JSON.stringify(FILES_RENAMED, 4)}`);
+	Promise.all(commits.map(processCommit)).then(() => {
+		core.debug(JSON.stringify(FILES, 4));
 
-	fs.writeFileSync(`${process.env.HOME}/files.json`, JSON.stringify(FILES), 'utf-8');
-	fs.writeFileSync(`${process.env.HOME}/files_modified.json`, JSON.stringify(FILES_MODIFIED), 'utf-8');
-	fs.writeFileSync(`${process.env.HOME}/files_added.json`, JSON.stringify(FILES_ADDED), 'utf-8');
-	fs.writeFileSync(`${process.env.HOME}/files_deleted.json`, JSON.stringify(FILES_DELETED), 'utf-8');
-	fs.writeFileSync(`${process.env.HOME}/files_renamed.json`, JSON.stringify(FILES_RENAMED), 'utf-8');
+		core.setOutput('all', JSON.stringify(FILES, 4));
+		core.setOutput('added', JSON.stringify(FILES_ADDED, 4));
+		core.setOutput('deleted', JSON.stringify(FILES_DELETED, 4));
+		core.setOutput('modified', JSON.stringify(FILES_MODIFIED, 4));
+		core.setOutput('renamed', JSON.stringify(FILES_RENAMED, 4));
 
-	process.exit(0);
+		fs.writeFileSync(`${process.env.HOME}/files.json`, JSON.stringify(FILES), 'utf-8');
+		fs.writeFileSync(`${process.env.HOME}/files_modified.json`, JSON.stringify(FILES_MODIFIED), 'utf-8');
+		fs.writeFileSync(`${process.env.HOME}/files_added.json`, JSON.stringify(FILES_ADDED), 'utf-8');
+		fs.writeFileSync(`${process.env.HOME}/files_deleted.json`, JSON.stringify(FILES_DELETED), 'utf-8');
+		fs.writeFileSync(`${process.env.HOME}/files_renamed.json`, JSON.stringify(FILES_RENAMED), 'utf-8');
+
+		process.exit(0);
+	});
 });
+
